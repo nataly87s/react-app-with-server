f40e5d24ec8e0cac93143538adccd5fbf15592f4
Nataly Shrits <nataly@soluto.com>
2017-06-02 19:35:44 +0300

    add proxy for api requests

--- server/index.js
+++ server/index.js
@@ -2,15 +2,9 @@ import express from 'express';
 import path from 'path';
 import nconf from 'nconf';
 
-nconf.argv().env().defaults({ NODE_ENV: 'development', PORT: '3001', WEBPACK_SERVER: 'http://localhost:3000'});
+nconf.argv().env().defaults({ PORT: '3001'});
 
-const NODE_ENV = nconf.get('NODE_ENV').toLowerCase();
 const PORT = nconf.get('PORT');
-const WEBPACK_SERVER = nconf.get('WEBPACK_SERVER');
-const isProduction = NODE_ENV === 'production';
-
-console.log('NODE_ENV', NODE_ENV);
-!isProduction && console.log('WEBPACK_SERVER', WEBPACK_SERVER);
 
 const app = express();
 
@@ -21,11 +15,7 @@ app.use("/api", (req, res) => {
 });
 
 app.get('/*', (req, res) => {
-    if (isProduction) {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    } else {
-        res.redirect(WEBPACK_SERVER + req.path);
-    }
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const server = app.listen(PORT, () => console.log('Listening on port %d', server.address().port));
--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            request: '',
+        };
+    }
+
+    onClick = () => {
+        fetch('/api').then(result => result.text()).then(request => this.setState({ request }));
+    };
+
   render() {
     return (
       <div className="App">
@@ -13,6 +25,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          <button onClick={this.onClick}>Click ME!</button>
+          <p>{this.state.request}</p>
       </div>
     );
   }
